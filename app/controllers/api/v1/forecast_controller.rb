292d2dad36f30520394f9f17a900e3b718c17788
Ethan Black <ethanrossblack@gmail.com>
2023-09-26 20:43:26 -0600

    fix: change controller inheritance

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,6 @@
-class Api::V1::ForecastController < ApplicationController
-
+class Api::V1::ForecastController < Api::V1::ApplicationController
   def index
     facade = ForecastFacade.new(params).forecast
     render json: ForecastSerializer.new(facade)
   end
-
 end
