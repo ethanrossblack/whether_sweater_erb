43f050e1f0948235e65e2864e4f1865bb93e7282
Ethan Black <ethanrossblack@gmail.com>
2023-09-26 22:37:30 -0600

    feat: add not found error

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ApplicationController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessible_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def render_unprocessible_entity_response(exception)
     render json: {
@@ -11,5 +12,17 @@ class Api::V1::ApplicationController < ApplicationController
         }
       end
     }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    render json: {
+      errors: [
+        {
+          status: "404",
+          title: "User Not Found",
+          detail: "Couldn't find a User with the given email address"
+        }
+      ]
+    }, status: :not_found
   end
 end
